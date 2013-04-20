9fd494ca422dbc59a80cf7d9551be88a850b2866
Michael Düll <mail@akurei.me>
2013-04-20 20:02:24 +0300

    Update hirnlogin.sh

--- hirnlogin.sh
+++ hirnlogin.sh
@@ -1,32 +1,50 @@
-#!/bin/bash
+#!/bin/sh
 
 #######################################################################
 # Modify this:                                                        #
+# Deine Daten eintragen:                                              #
 #######################################################################
-_USER='foo' # Login-ID, e.g. meiera9d
-_PASS='bar' # Password
+_USER='testus1' # Username (Login-ID) / Benutzer (Login-ID)
+_PASS='mypassword123' # Password / Passwort
 
 #######################################################################
-# Don't change anything past this                                     #
+# Don't change anything past this line!                               #
+# Nach dieser Zeile nichts mehr ändern!                               #
 #######################################################################
-_USERAGENT='Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/12.0'
+_USERAGENT='HIRN Login Script v0.2'
 _STARTURL='https://login.rz.ruhr-uni-bochum.de/cgi-bin/start'
 _POSTURL='https://login.rz.ruhr-uni-bochum.de/cgi-bin/laklogin' 
 _CACERT='/etc/ssl/certs/Deutsche_Telekom_Root_CA_2.pem' 
 _POST="loginid=$_USER&password=$_PASS&action=Login"
+_ISINTERNETUP='google.com'
+_CHECKSTRING='des Zugangs Ihre Identifikation und das zugeh&ouml;rige Passwort ein.'
+_SUCCESSSTRING='gelungen'
 
-curl -s -1 -4 -A "$_USERAGENT" --cacert "$_CACERT" "$_STARTURL" | grep -q "des Zugangs Ihre Identifikation und das zugeh&ouml;rige Passwort ein."
+# Check if we can reach "the internet". If we're already online, we exit.
+ping -W 1 -c 1 $_ISINTERNETUP >/dev/null 2>&1
 _EXIT=$?
-if [ $_EXIT -ne 0 ]; then
-  echo $_EXIT
-  exit $_EXIT
+if [ $_EXIT -eq 0 ]; then
+    echo "Already logged in: Exiting..."
+    exit 0
 fi
-curl -s -1 -4 -A "$_USERAGENT" -d "$_POST" -e "$_STARTURL" -cacert "$_CACERT" "$_POSTURL" | grep -q "gelungen"
+
+# Check if we're really at a HIRN Port.
+curl -s -1 -4 -A "$_USERAGENT" --cacert "$_CACERT" "$_STARTURL" 2>/dev/null | grep -q "$_CHECKSTRING"
 _EXIT=$?
 if [ $_EXIT -ne 0 ]; then
-  echo $_EXIT
-  exit $_EXIT
+  echo "Cannot reach HIRN-Port."
+  exit 2
+fi
+
+# Do the Login
+curl -s -1 -4 -A "$_USERAGENT" -d "$_POST" -e "$_STARTURL" --cacert "$_CACERT" "$_POSTURL" | grep -q "$_SUCCESSSTRING"
+_EXIT=$?
+if [ $_EXIT -ne 0 ]; then
+  echo "Login failed!"
+  exit 1
 fi
 
 unset _USER
 unset _PASS
+
+exit 0
